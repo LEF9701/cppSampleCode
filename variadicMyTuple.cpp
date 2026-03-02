898cd87eca663f2f4b4e0a799582e9e33655cc4b
Lawrence/Laurie <law41592@gmail.com>
2026-03-02 14:55:07 +0800

    Update variadicMyTuple.cpp

--- variadicMyTuple.cpp
+++ variadicMyTuple.cpp
@@ -1,3 +1,7 @@
+#include<cstddef>
+#include<iostream>
+
+
 template<typename... Args>
 class MyTuple;
 
@@ -8,6 +12,8 @@ template<typename T, typename... Rest>
 class MyTuple<T, Rest...> {
     T value;
     MyTuple<Rest...> rest;
+
+    template<size_t, typename> friend struct TupleGet;
 
 public:
     MyTuple(T v, Rest... r)
@@ -31,3 +37,42 @@ MyTuple<int, double, char>
                         MyTuple<>
 
 */
+
+// Base case: index 0 returns the current value
+template<size_t I, typename Tuple>
+struct TupleGet;
+
+template<typename T, typename... Rest>
+struct TupleGet<0, MyTuple<T, Rest...>> {
+    static T& get(MyTuple<T, Rest...>& t) {
+        return t.value;
+    }
+};
+
+// Recursive case: peel off one element
+template<size_t I, typename T, typename... Rest>
+struct TupleGet<I, MyTuple<T, Rest...>> {
+    static auto& get(MyTuple<T, Rest...>& t) {
+        return TupleGet<I - 1, MyTuple<Rest...>>::get(t.rest);
+    }
+};
+
+// Convenience function
+template<size_t I, typename... Args>
+auto& get(MyTuple<Args...>& t) {
+    return TupleGet<I, MyTuple<Args...>>::get(t);
+}
+
+
+
+int main(){
+    MyTuple<int, double, char> t(1, 3.14, 'a');
+    auto id_0 = get<0>(t); // 1
+    auto id_1 = get<1>(t); // 3.14
+    auto id_2 = get<2>(t); // 'a'
+    std::cout << "get<0>t : " << id_0 << std::endl;
+    std::cout << "get<1>t : " << id_1 << std::endl;
+    std::cout << "get<2>t : " << id_2 << std::endl;
+
+    return 0;    
+}
