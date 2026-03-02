f0f173fdcfcadf01dd80b73f0e7a13dc75150399
Lawrence/Laurie <law41592@gmail.com>
2026-03-02 15:35:09 +0800

    Update variadicMyTuple.cpp

--- variadicMyTuple.cpp
+++ variadicMyTuple.cpp
@@ -16,6 +16,8 @@ class MyTuple<T, Rest...> {
     T value;
     MyTuple<Rest...> rest;
 
+    // TupleGet requires access to private members of MyTuple<T, Rest...>
+    // for this MyTuple<...> specialization, all TupleGet<I, Something> specializations are friends (so they can access private members)
     template<size_t, typename> friend struct TupleGet;
 
 public:
@@ -41,10 +43,10 @@ MyTuple<int, double, char>
 
 */
 
-// Base case: index 0 returns the current value
 template<size_t I, typename Tuple>
 struct TupleGet;
 
+// Base case: index 0 returns the current value
 template<typename T, typename... Rest>
 struct TupleGet<0, MyTuple<T, Rest...>> {
     static T& get(MyTuple<T, Rest...>& t) {
@@ -60,7 +62,15 @@ struct TupleGet<I, MyTuple<T, Rest...>> {
     }
 };
 
-// Convenience function
+/*
+So get<2>(t) expands like:
+TupleGet<2, MyTuple<int,double,char>>::get(t)
+-> calls TupleGet<1, MyTuple<double,char>>::get(t.rest)
+-> calls TupleGet<0, MyTuple<char>>::get(t.rest.rest)
+-> returns char& from that node’s value
+*/
+
+// Convenience function(just a wrapper)
 template<size_t I, typename... Args>
 auto& get(MyTuple<Args...>& t) {
     return TupleGet<I, MyTuple<Args...>>::get(t);
@@ -70,9 +80,9 @@ auto& get(MyTuple<Args...>& t) {
 
 int main(){
     MyTuple<int, double, char> t(1, 3.14, 'a');
-    auto id_0 = get<0>(t); // 1
-    auto id_1 = get<1>(t); // 3.14
-    auto id_2 = get<2>(t); // 'a'
+    decltype(auto) id_0 = get<0>(t); // 1
+    decltype(auto) id_1 = get<1>(t); // 3.14
+    decltype(auto) id_2 = get<2>(t); // 'a'
     std::cout << "get<0>t : " << id_0 << std::endl;
     std::cout << "get<1>t : " << id_1 << std::endl;
     std::cout << "get<2>t : " << id_2 << std::endl;
