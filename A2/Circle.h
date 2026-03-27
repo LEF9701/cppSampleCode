915ae42c70a088e1223bfafc3adc26a7cd3e7ed7
Lawrence/Laurie <law41592@gmail.com>
2026-03-27 15:11:32 +0800

    Create Circle.h

---
+++ A2/Circle.h
@@ -0,0 +1,15 @@
+#ifndef CIRCLE_H
+#define CIRCLE_H
+#include "Shape.h"
+
+class Circle : public Shape{
+    private:
+        double radius;
+    public:
+        Circle(double r);
+        ~Circle();
+        double getArea();
+        void display();
+};
+
+#endif
