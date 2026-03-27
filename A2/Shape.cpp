111a800304d40f156f9cd34c5397e341113ab622
Lawrence/Laurie <law41592@gmail.com>
2026-03-27 15:13:56 +0800

    Delete A2 directory

--- A2/Shape.cpp
+++
@@ -1,9 +0,0 @@
-#include<iostream>
-#include<string>
-#include"Shape.h"
-
-Shape::Shape(std::string name): name(name){}
-Shape::~Shape(){std::cout << "Shape " << getName() << " deleted." << std::endl;}
-std::string Shape::getName(){return name;}
-
-
